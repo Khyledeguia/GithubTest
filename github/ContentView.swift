53b16879e7b4a2ea8f5c8ad54929fd8abc7b258d
Khyle <khyle.deguia@powermaccenter.com>
2023-02-22 09:14:20 +0800

    Change some text and comment

--- github/ContentView.swift
+++ github/ContentView.swift
@@ -1,21 +1,23 @@
-//
-//  ContentView.swift
-//  github
-//
-//  Created by Khyle De Guia on 2/22/23.
-//
+    //
+    //  ContentView.swift
+    //  github
+    //
+    //  Created by Khyle De Guia on 2/22/23.
+    //
 
-import SwiftUI
+    import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    //helloooooo
+
+    struct ContentView: View {
+        var body: some View {
+            Text("Khyle De Guia testing Github")
+                .padding()
+        }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
