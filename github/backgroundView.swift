2e34d187ad28c7e58d5a9013788ada9e3be87720
Khyle <khyle.deguia@powermaccenter.com>
2023-02-22 09:45:05 +0800

    creates second view

---
+++ github/backgroundView.swift
@@ -0,0 +1,20 @@
+//
+//  backgroundView.swift
+//  github
+//
+//  Created by Khyle De Guia on 2/22/23.
+//
+
+import SwiftUI
+
+struct backgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct backgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        backgroundView()
+    }
+}
