a2d8f05466c03350f97b3b6065f80b66711b799e
Khyle <khyle.deguia@powermaccenter.com>
2023-02-22 09:10:58 +0800

    Initial Commit

---
+++ github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github
+//
+//  Created by Khyle De Guia on 2/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Khyle De Guia on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
